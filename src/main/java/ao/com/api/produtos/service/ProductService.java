28c1a187658e46ec031e0dd5eaae7f1e5e40bf7a
dannydagamaisutic@gmail.com <dannydagamaisutic@gmail.com>
2022-12-23 18:34:15 +0100

    Implementação do metodo para mostrar Produtos

--- src/main/java/ao/com/api/produtos/service/ProductService.java
+++ src/main/java/ao/com/api/produtos/service/ProductService.java
@@ -1,5 +1,16 @@
 package ao.com.api.produtos.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import ao.com.api.produtos.models.ProductsModels;
+import ao.com.api.produtos.repository.ProductRepository;
+
+@Service
 public class ProductService {
-    
+    @Autowired
+    private ProductRepository pr;
+    public Iterable<ProductsModels> listar(){
+        return pr.findAll();
+    }
 }
