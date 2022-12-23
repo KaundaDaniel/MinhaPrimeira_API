4dea3c97da27972d9fb5ea4b7e98221f40ad6b1b
dannydagamaisutic@gmail.com <dannydagamaisutic@gmail.com>
2022-12-23 19:10:35 +0100

    Listar produtos

--- src/main/java/ao/com/api/produtos/controls/ProductsControls.java
+++ src/main/java/ao/com/api/produtos/controls/ProductsControls.java
@@ -1,10 +1,25 @@
 package ao.com.api.produtos.controls;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import ao.com.api.produtos.models.ProductsModels;
+import ao.com.api.produtos.service.ProductService;
+
+
+
 @RestController
 public class ProductsControls{
+@Autowired
+private ProductService ps;
+@GetMapping("/listar")
+public Iterable <ProductsModels> listar (){
+ return ps.listar();
+}
+
+
+
     @GetMapping("/")
 public String rota(){
     return"rota Funcionando";
